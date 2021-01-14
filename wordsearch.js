154bc7aa930160fd15da1705ce5f53f22d1b1968
Jinny Jang <jangjinny21@gmail.com>
2021-01-14 00:00:29 +0000

    function to check if the word is found in the array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const filteredArr = function(letters) {
+//converts the vertical line into a horizontal array
+  let newArr = [];
+  const row = letters.length;
+  const col = letters[0].length;
+    
+  for (let i = 0; i < col; i++) {
+    newArr.push([]);
+  }
+    
+  for (let x = 0; x < row; x++) {
+    for (let y = 0; y < col; y++) {
+      newArr[y].push(letters[x][y]);
     }
-}
+  }
+  
+  return newArr;
 
-module.exports = wordSearch
+};
+
+const wordSearch = (letters, word) => {
+  const verticalJoin = filteredArr(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+module.exports = wordSearch;
